8a9f4f9781cdcb7b2b75cec5252d10285d2d7775
aleixrc7 <aleixasir@gmail.com>
2022-02-25 21:51:45 +0100

    feat: javadoc implementado

--- src/com/ud8/entornos/EntradaBlogARC.java
+++ src/com/ud8/entornos/EntradaBlogARC.java
@@ -1,5 +1,11 @@
 package com.ud8.entornos;
 
+/**
+ * <h1>Clase empleada para sacar la entrada de un blog, ejercicio de ED</h1>
+ * @author aleix
+ *
+ */
+
 public class EntradaBlogARC {
 	//separador es el ccarácter que separa ENTRADA DE del
 	//nombre del autor
@@ -9,15 +15,28 @@ public class EntradaBlogARC {
 	private String texto;
 	private String autor;
 	
-	//Constructor de la clase. recibe el número de entrada, el nombre del autor
-	//de la entrada y el texto que contiene la entrada. Si el número de entrada
-	//es negativo, lanza una excepción.
+	
+	/**
+	 * 
+	 * @param id    <i>Id de la entrada</i>
+	 * @param autor <i>Nombre del autor</i>
+	 * @param texto <i>Texto introducido por el autor</i>
+	 * @throws IllegalArgumentException
+	 * 
+	 * Constructor de la clase
+	 */
+	
 	public EntradaBlogARC(int id,String autor,String texto)throws IllegalArgumentException{
 		if(id<=0) throw new IllegalArgumentException("El id no puede ser negativo");
 		this.id=id;
 		this.autor=autor;
 		this.texto=texto;
 	}
+	
+	/**
+	 * Sobreescrito el motodo toString para devolver valores al 
+	 * hacer println sobre el objeto
+	 */
 	@Override
 	public String toString(){
 		String cad="";
@@ -26,22 +45,45 @@ public class EntradaBlogARC {
 		return cad;
 	}
 	
-	//Devuelve el número de la entrada
+	
+	/**
+	 * 
+	 * @return <ul>
+	 * 				<li>el número de la entrada </li>
+	 * 		   </ul>
+	 */
+	
 	public int getId(){
 		return this.id;
 	}
 	
-	//devuelve el texto completo de la entrada
+	
+	/**
+	 * 
+	 * @return <ul>
+	 * 				<li> devuelve el texto completo de la entrada </li>
+	 * 		   </ul>
+	 */
 	public String getTexto(){
 		return this.texto;
 	}
 	
-	//devuelve el nombre del autor de la entrada en mayúsculas
+	/**
+	 * 
+	 * @return <ul>
+	 * 				<li> devuelve el nombre del autor de la entrada en mayúsculas </li>
+	 * 		   </ul>
+	 */
 	public String getAutor(){
 		return this.autor.toUpperCase();
 	}
 	
+	
 	//devuelve el nombre del autor. Ya no se usa. Mejor ver getAutor
+	/**
+	 * 
+	 * @deprecated 
+	 */
 	public String devuelveAutor(){
 		return this.autor;
 	}
